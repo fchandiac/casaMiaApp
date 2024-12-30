538af2973ca29a6b7c4d838a296326a3f0560262
Felipe Chandía <felipe@MacBook-Pro-de-Felipe-2.local>
2024-12-30 01:03:40 -0300

    restore build

--- app/userApp/index.tsx
+++ app/userApp/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import Title from "../../components/commons/Title";
 import UserMissionminiCard from "../../components/missions/UserMissionminiCard";
 import { useAuth0 } from "react-native-auth0";
@@ -13,6 +19,7 @@ export default function Index() {
   const { findAccountByEmail } = account;
   const { getUserMissions } = mission;
   const [missionsList, setMissionsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Estado para manejar la carga
 
   useEffect(() => {
     const fetchMissions = async () => {
@@ -20,34 +27,47 @@ export default function Index() {
         const userAccount = await findAccountByEmail(user.email);
         const missions = await getUserMissions(userAccount.id);
         setMissionsList(missions);
+        setIsLoading(false); // Datos cargados, desactivar el loading
       } catch (error) {
         console.error("Error fetching missions:", error);
+        setIsLoading(false); // Si hay un error, también desactivar el loading
       }
     };
 
     fetchMissions();
   }, [user]);
+
   return (
     <View>
       <Title title="Misiones" />
-      <ScrollView horizontal={true}>
-        {missionsList.map((mission, index) => (
-          <Pressable
-            key={index}
-            onPress={() => router.push("/userApp/missions/" + mission.id)}
-          >
-            <UserMissionminiCard
+
+      {/* Si los datos aún se están cargando, mostrar un indicador de carga */}
+      {isLoading ? (
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          style={{ marginTop: 50 }}
+        />
+      ) : (
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {missionsList.map((mission, index) => (
+            <Pressable
               key={index}
-              id={mission.id}
-              name={mission.name}
-              description={mission.description}
-              points={mission.points}
-              clp={mission.clp}
-              imageUrl={mission.imageUrl}
-            />
-          </Pressable>
-        ))}
-      </ScrollView>
+              onPress={() => router.push("/userApp/missions/" + mission.id)}
+            >
+              <UserMissionminiCard
+                key={index}
+                id={mission.id}
+                name={mission.name}
+                description={mission.description}
+                points={mission.points}
+                clp={mission.clp}
+                imageUrl={mission.imageUrl}
+              />
+            </Pressable>
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 }
